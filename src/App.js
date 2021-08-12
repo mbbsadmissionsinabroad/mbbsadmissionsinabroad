b233f06bfb645538174d6a95471fb8ff92b6e850
mbbsadmissionsinabroad <mbbsadmissionsinabroad@gmail.com>
2021-08-12 16:01:42 +0530

    Changes in Tawk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import ScrollToTop from "react-scroll-to-top";
 import { BrowserRouter as Router} from "react-router-dom";
@@ -7,10 +7,17 @@ import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Routes from "./Routes";
-import TawkComponent from "./components/TawkComponent";
-
+// import TawkComponent from "./components/TawkComponent";
+import tawkTo from "tawkto-react";
 
 function App() {
+
+  const tawkToPropertyId = '6114f086649e0a0a5cd0c9cd';
+  const tawkToKey = 'c1750d87345da8d2071f35c771ab10bdcb7ab9dd';
+  useEffect(() => {
+    tawkTo(tawkToPropertyId, tawkToKey)
+  }, []);
+
   return (
     <div className="app">
       <Router>
@@ -18,7 +25,7 @@ function App() {
         <ScrollToTop smooth Top/>
         <NavBar />
         <Routes />
-        <TawkComponent />
+        {/* <TawkComponent /> */}
         <Footer />
       </Router>
     </div>
